84242f8d62b0d3c84396ce0b7331dadf48ed8911
Sahil Jaiswal <sahiljaiswal166@gmail.com>
2025-04-22 18:04:18 +0530

    Creatint Element in React

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,9 +1,13 @@
 import Chai from './chai'
 
 function App() {
+  const username = "chai aur code"
+  
   return (
     <>           
       <Chai/>
+      <h1>React {username}</h1>
+      <p>Test Paragraph</p>
     </> 
   )
 }
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,7 +1,41 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import React from 'react'
+
+function MyApp(){
+    return (
+        <div>
+            <h1>Custom App ! SRJ</h1>
+        </div>
+    )
+}
+
+// const ReactElement = {
+//     type: 'a',
+//     props: {
+//         href: 'https://google.com',
+//         target: '_blank'
+//     },
+//     children: 'Click me to visit Google.'
+// }
+
+
+const anotherUser = "sahil"
+
+const ReactElement = React.createElement(
+    'a', 
+    {href: "https://google.com", target: '_blank'},
+    'Click me to visit Google',
+    anotherUser       // evaluated expression
+)
+
+const anotherElement = (
+    <a href="https://google.com" target='_blank'>Visit Google</a>
+)
+
+
 
 createRoot(document.getElementById('root')).render(
-    <App />
+    ReactElement
 )
